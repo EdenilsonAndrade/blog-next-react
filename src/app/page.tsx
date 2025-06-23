4393f25c52e317386cd7d248f130167469a0f714
EdenilsonAndrade <edenilsonbass@hotmail.com>
2025-06-23 05:14:56 -0300

    feat: add SpinLoader componet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Header } from '@/components/Header';
+import { SpinLoader } from '@/components/SpinLoader';
+import clsx from 'clsx';
 
 export default async function HomePage() {
   console.log('HomePage component rendered');
   return (
     <div>
-      <Header />
+      <SpinLoader containerClasses={clsx('min-h-[500px]', 'bg-amber-500')} />
     </div>
   );
 }
