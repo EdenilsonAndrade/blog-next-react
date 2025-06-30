e7e32538eb279c52630d326a0a0be9557b9d5c1d
EdenilsonAndrade <edenilsonbass@hotmail.com>
2025-06-30 04:46:26 -0300

    feat: add PostHeading component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
+import { PostHeading } from '@/components/PostHeading';
 import { PostsList } from '@/components/PostsLIst';
 import { SpinLoader } from '@/components/SpinLoader';
 import Image from 'next/image';
@@ -14,18 +15,32 @@ export default async function HomePage() {
       <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
         <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
           <Image
-            className='group-hover:scale-105 transition'
+            className='w-full h-full object-cover object-center group-hover:scale-105 transition'
             src='/images/bryen_0.png'
             width={1200}
             height={720}
             alt='Título do post'
+            priority
           />
         </Link>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos
-          molestiae, illum magnam reprehenderit, dolorum, et impedit rem est
-          odit veniam sint amet eum facilis temporibus labore. Incidunt
-          voluptatem accusantium perferendis?
+        <div className='flex flex-col gap-4 sm:justify-center'>
+          <time
+            className='text-slate-600 block text-sm/tight'
+            dateTime='2025-06-30'
+          >
+            30/06/2025 10:00
+          </time>
+
+          <PostHeading as='h1' url='#'>
+            Incidunt voluptatem accusantium perferendis?
+          </PostHeading>
+
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos
+            molestiae, illum magnam reprehenderit, dolorum, et impedit rem est
+            odit veniam sint amet eum facilis temporibus labore. Incidunt
+            voluptatem accusantium perferendis?
+          </p>
         </div>
       </section>
 
